0fbe42d554da7251a6138538b7ff685c9e745fd0
GouravSharma26 <gourav123sharma4567@gmail.com>
2026-08-18 23:04:03 +0530

    One-time: delete all accounts on next deploy

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# ONE-TIME: Delete all accounts (REMOVE THIS AFTER DEPLOY)
+python manage.py shell -c "from core.models import User; count = User.objects.count(); User.objects.all().delete(); print(f'Deleted {count} accounts')"
