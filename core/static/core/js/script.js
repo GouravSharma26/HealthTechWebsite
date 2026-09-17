ab573a97176ed5cca5b0a266b32aafc1ff565a99
GouravSharma26 <gourav123sharma4567@gmail.com>
2026-09-17 19:55:15 +0530

    feat(ui): implement designed empty states for doctor and chat profiles

--- core/static/core/js/script.js
+++ core/static/core/js/script.js
@@ -368,6 +368,11 @@
     document.querySelector(`[onclick="showTab('${type}')"]`).classList.add('active');
   }
   
-
-  
-  
+  function toggleTheme() {
+    const root = document.documentElement;
+    const currentTheme = root.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    
+    root.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+  }
